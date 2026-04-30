2a682df0427a4cdb3e50745694e9a91abf82b06e
kzh4295 <kzh4295@naver.com>
2026-04-30 14:05:01 +0900

    refactor: 레거시 파일 삭제

--- src/api.ts
+++
@@ -1,9 +0,0 @@
-import type { ContentItem } from './types';
-
-const API_URL = 'https://closet-recruiting-api.azurewebsites.net/api/data';
-
-export const fetchContents = async (): Promise<ContentItem[]> => {
-  const response = await fetch(API_URL);
-  const data: ContentItem[] = await response.json();
-  return data;
-};
